3643504db7278917be01b27560d37fe599337456
Bryan Bancroft <bryanbancroft@gmail.com>
2019-04-27 20:07:20 -0400

    Constants added

---
+++ app/src/main/java/edu/temple/mobiledevgroupproject/Objects/Constants.java
@@ -0,0 +1,11 @@
+package edu.temple.mobiledevgroupproject.Objects;
+
+public class Constants {
+
+    public static final String ROOT_URL = "http://192.168.1.158:80/volunteer_app/v1/";
+    public static final String SIGN_UP_URL = ROOT_URL + "registerUser.php";
+    public static final String LOGIN_URL = ROOT_URL + "userLogin.php";
+    public static final String NEW_JOB_URL = ROOT_URL + "registerJob.php";
+    public static final String GET_JOBS_URL = ROOT_URL + "getJobs.php";
+
+}
