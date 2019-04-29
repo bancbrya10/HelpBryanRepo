79835aff7dc5e4fbeb2f3102015f4905a1876610
Bryan Bancroft <bryanbancroft@gmail.com>
2019-04-29 08:09:22 -0400

    Add and get comments added to jobviewactivity and commentfragment

--- app/src/main/java/edu/temple/mobiledevgroupproject/Objects/Constants.java
+++ app/src/main/java/edu/temple/mobiledevgroupproject/Objects/Constants.java
@@ -2,7 +2,7 @@ package edu.temple.mobiledevgroupproject.Objects;
 
 public class Constants {
 
-    public static final String ROOT_URL = "http://192.168.1.158:80/volunteer_app/v1/";
+    public static final String ROOT_URL = "http://10.109.7.32:80/volunteer_app/v1/";
     public static final String SIGN_UP_URL = ROOT_URL + "registerUser.php";
     public static final String LOGIN_URL = ROOT_URL + "userLogin.php";
     public static final String NEW_JOB_URL = ROOT_URL + "registerJob.php";
